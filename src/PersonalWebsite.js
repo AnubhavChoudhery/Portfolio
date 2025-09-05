abe253f6c074688aea6d21f53449664518b9e2d1
Anubhav Choudhery <anubhavchoudhery@gmail.com>
2025-09-05 13:58:09 -0500

    Updated skills and about me

--- src/PersonalWebsite.js
+++ src/PersonalWebsite.js
@@ -138,13 +138,14 @@ const PersonalWebsite = () => {
             </p>
             <p className="about-content">
               I've also had the privilege to work in research on the crossroads of Machine Learning and Cybersecurity (within the MLOPT Research Group), and 
-              lead an engineering student origanization (Wisconsin Robotics) with more than 40 members. My recent professional experience also 
+              lead multiple engineering student origanizations (Wisconsin Robotics, RoboKnights) with more than 40 members. My recent professional experience also 
               includes an internship as Cybersecurity Analyst at Culligan International, where I worked alongside the Global Cybersecurity team
               to build python-based tools for automating manual processes, reporting directly to the global CISO. 
             </p>
             <p className="about-content">
-              Aside from my technical pursuits, I enjoy playing chess (<a href = "https://lichess.org/@/anubhav95">Challenge Me!</a>), 
-              drums (learnt up to Grade 3), ping pong (table tennis) and pickleball. 
+              Aside from my technical pursuits, I enjoy playing chess (<a href = "https://lichess.org/@/anubhav95" target="_blank">Challenge Me!</a>), 
+              drums (learnt up to Grade 3), ping pong (table tennis) and pickleball. My cinematic interests typically include action, 
+              comedy and sci-fi movies with Mission Impossible and Star Wars being my favorite collections.
               These activities keep me balanced, sharpen my problem-solving skills while also improving quick thinking, and give me space to recharge creatively.
             </p>
           </div>
